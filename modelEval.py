1f8c0e272137a39fcf61eff5542b046e691a3627
aayushs-edu <aayushs2008@gmail.com>
2024-11-07 20:36:39 -0800

    Model Eval File

---
+++ modelEval.py
@@ -0,0 +1,14 @@
+import joblib
+import pandas as pd
+
+# Load the saved model
+model = joblib.load('model.pkl')
+
+df = pd.read_csv('evalData.csv')
+first_row = df.drop("diseases", axis=1).iloc[0]
+print(first_row)
+
+# Make a prediction using the model
+prediction = model.predict([first_row])
+print(str(prediction))
+
