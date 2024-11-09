1567c947fed4f85fb22cd40ce78ee65ab5821d79
Aarush Parikh <parikhaarush@gmail.com>
2024-11-08 19:34:14 -0800

    seq tester created

--- modelEval.py
+++ modelEval.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
 
 # Load the saved model
-model = joblib.load('model.pkl')
+model = joblib.load('rfc_modelV2.pkl')
 
 df = pd.read_csv('evalData.csv')
 first_row = df.drop("diseases", axis=1).iloc[0]
